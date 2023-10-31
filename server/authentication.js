55620b6efcd548ab81577c406ee677f44f30b63a
RickLiu <rick.liu0121@gmail.com>
2023-10-31 16:00:38 -0400

    Working on Authentication

---
+++ server/authentication.js
@@ -0,0 +1,34 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+ */
+
+var express = require("express"); // Express web server framework
+var request = require("request"); // "Request" library
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+app = express();
+var redirect_uri = "http://localhost:3000/callback"; // Your redirect uri
+var client_id = "74279caccabd4f18b9d28b4c6f37d000";
+app.get("/login", function (req, res) {
+  var state = generateRandomString(16);
+  var scope = "user-read-private user-read-email";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
+console.log("Listening on 3000");
+app.listen(3000);
