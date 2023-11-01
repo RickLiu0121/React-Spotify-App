4def4c4a0a7552968bfdd09102532fc553b0cb83
RickLiu <rick.liu0121@gmail.com>
2023-11-01 14:50:02 -0400

    Added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from "react";
+import WebPlayback from "./WebPlayback";
 import Login from "./Login";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  return <Login />;
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+  }, []);
+
+  return <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>;
 }
 
 export default App;
