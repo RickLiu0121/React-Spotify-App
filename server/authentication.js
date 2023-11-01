ffa274a8ea949914a209c98141d176cb883daaa3
RickLiu <rick.liu0121@gmail.com>
2023-11-01 14:14:17 -0400

    Refactor

--- server/authentication.js
+++ server/authentication.js
@@ -1,34 +1,81 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
+const express = require("express");
+const dotenv = require("dotenv");
+const port = 3000;
+dotenv.config();
 
-var express = require("express"); // Express web server framework
-var request = require("request"); // "Request" library
-var cors = require("cors");
-var querystring = require("querystring");
-var cookieParser = require("cookie-parser");
-app = express();
-var redirect_uri = "http://localhost:3000/callback"; // Your redirect uri
-var client_id = "74279caccabd4f18b9d28b4c6f37d000";
-app.get("/login", function (req, res) {
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+var app = express();
+
+var generateRandomString = function (length) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+app.get("/auth/login", (req, res) => {
+  var scope =
+    "streaming \
+               user-read-email \
+               user-read-private";
+
   var state = generateRandomString(16);
-  var scope = "user-read-private user-read-email";
+
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: "http://localhost:3000/auth/callback",
+    state: state,
+  });
 
   res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state,
-      })
+    "https://accounts.spotify.com/authorize/?" +
+      auth_query_parameters.toString()
   );
 });
-console.log("Listening on 3000");
-app.listen(3000);
+// app.use((req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.get("/auth/callback", (req, res) => {
+  var code = req.query.code;
+
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: spotify_redirect_uri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(spotify_client_id + ":" + spotify_client_secret).toString(
+          "base64"
+        ),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token;
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/auth/token", (req, res) => {
+  res.json({ access_token: access_token });
+});
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
